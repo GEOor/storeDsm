db575974d4502d739a69eb93c6b262b2b2b7b1c9
brorica <minjea917@gmail.com>
2022-05-25 21:07:20 +0900

    DSM x,y,z 타입 변경 (#2)
    * Feat: DSM 컬럼 타입 변경
    
    equal 연산을 위해 double 타입에서 string으로 변경

---
+++ dsm.sql
@@ -0,0 +1,6 @@
+create table dsm(
+   x varchar(32) not null,
+   y varchar(32) not null,
+   z varchar(32) not null,
+   sig_cd char(2) default '00'
+)
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,9 @@ import Service.DsmService;
 import java.sql.SQLException;
 
 public class Main {
-    
-    private static DsmService dsmService = new DsmService();
-    private static String rootPath = "src/Files";
-    
+
     public static void main(String[] args) {
-    
-        long startTime = System.currentTimeMillis();
         DsmService dsmService = new DsmService();
         dsmService.save();
-
-        long endTime = System.currentTimeMillis();
-        System.out.println(String.format("코드 실행 시간: %20dms", endTime - startTime));
     }
 }
